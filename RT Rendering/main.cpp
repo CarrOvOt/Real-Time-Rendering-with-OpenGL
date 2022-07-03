1d9ae3cd98fe4f589c639d1d53c54e5ad84a566f
CarrOvOt <969042954@qq.com>
2022-07-03 17:14:23 +0800

    build base environment

---
+++ RT Rendering/main.cpp
@@ -0,0 +1,41 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+
+#include <iostream>
+
+int main(){
+
+    // initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // create a window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    // initialize GLAD *AFTER* creating a GLFWindow
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // rendering loop
+    while (!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+    // release resource and quit
+    glfwTerminate();
+    return 0;
+}
