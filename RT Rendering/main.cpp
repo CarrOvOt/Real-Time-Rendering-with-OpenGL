6c7387082febd79c963540daa3661d6e2f968437
CarrOvOt <969042954@qq.com>
2022-07-04 18:02:11 +0800

    render a cube simply

--- RT Rendering/main.cpp
+++ RT Rendering/main.cpp
@@ -28,11 +28,156 @@ int main(){
         return -1;
     }
 
+
+
+
+    //process mesh data
+
+    /*
+    
+      7---------6
+     /|        /|
+    4---------5 |
+    | |       | |                      y|
+    | 3-------|-2                       |____x
+    |/        |/                        /
+    0---------1                        /z
+    
+    */
+
+    // 8 vertices of a cube
+    float vertices[] = {
+        -0.5f,  -0.5f,  0.5f,
+        0.5f,   -0.5f,  0.5f,
+        0.5f,   -0.5f,  -0.5f,
+        -0.5f,  -0.5f,  -0.5f,
+        -0.5f,  0.5f,   0.5f,
+        0.5f,   0.5f,   0.5f,
+        0.5f,   0.5f,   -0.5f,
+        -0.5f,  0.5f,   -0.5f,
+    };
+
+    // 12 triangles of 6 face
+    unsigned int indices[] = {
+        0,1,4,  1,5,4,
+        1,2,5,  2,6,5,
+        2,3,6,  3,7,6,
+        3,0,7,  0,4,7,
+        4,5,7,  5,6,7,
+        2,3,1,  3,0,1,
+    };
+
+    //init VBO,VAO,EBO
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    //explain data buffer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0);
+
+
+
+    // create a shaer program
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n\0";
+
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);      // Wireframe Mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    // Default Mode
+
     // rendering loop
     while (!glfwWindowShouldClose(window)){
+
+        glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        //draw our mesh
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); 
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+
+    // release resource
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
 
     // release resource and quit
--- Readme.md
+++ Readme.md
@@ -2,12 +2,6 @@
 
 在window环境下使用openGL学习实时渲染相关的各种基础/算法/实现/技巧等。。。
 
-
-
-
-
-
-
 ### ver0.1
 
 搭建基础环境，能够显示出一个窗口。
@@ -17,5 +11,37 @@
 参考资料：[你好，窗口 - LearnOpenGL CN (learnopengl-cn.github.io)](https://learnopengl-cn.github.io/01%20Getting%20started/03%20Hello%20Window/)
 
 <img title="" src="MDImages/2022-07-03-17-36-00-image.png" alt="" width="404">
+
+
+
+### ver0.2
+
+以默认模式和线框模式（Wireframe Mode）显示一个立方体
+
+pipeline：
+
+- 创建并绑定VAO，VBO，EBO
+
+- 创建编译顶点着色器，片段着色器，链接
+
+- 主循环中渲染即可
+
+<img title="" src="MDImages/2022-07-04-17-52-40-image.png" alt="" width="310"> <img title="" src="MDImages/2022-07-04-17-53-19-image.png" alt="" width="310">
+
+现在我们只能看到立方体的一个面，而且是长方形不是正方形，这是因为目前没有对坐标进行各个空间上的变换（局部空间，世界空间，观察空间），这个问题之后再解决。
+
+
+
+### ver0.21
+
+TODO：整理代码，将着色器程序，顶点数据单独构建成类
+
+
+
+
+
+### ver0.3
+
+TODO：引入ImGui，方便后续各种参数的调试
 
 
